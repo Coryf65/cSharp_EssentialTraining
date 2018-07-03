98941a06ba7cea3b71b3c9f013422b5c838d3187
Cory <Coryf65@gmail.com>
2018-07-02 20:02:20 -0500

    Add project files.

---
+++ cSharp_UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("cSharp_UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("cSharp_UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bcaf10d7-fd3f-40e9-a423-958be4dbe96c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ cSharp_UnitTests/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using cSharp_EssentialTraining;
+
+namespace cSharp_UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            var testInstance = new Class1();          
+
+            var testResult = testInstance.AddTwo(9, 5);
+
+            Assert.AreEqual(14, testResult, "I Expect 9 + 5 to be 14");
+
+        }
+    }
+}
